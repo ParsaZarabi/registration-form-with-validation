14a9f8b063d2b7276cfe477515fd9dec9a05ac45
Parsa Zarabian <zarabiparsa@gmail.com>
2022-04-16 13:54:33 +0430

    Fixed some issues.

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import { validate } from './validate';
+import { notify } from './toast';
 
 const SignUp = () => {
 
@@ -12,11 +15,11 @@ const SignUp = () => {
         isAccepted: false
     })
     const [errors, setErrors] = useState({});
+    const [touched, setTouched] = useState({});
 
     useEffect(() => {
         setErrors(validate(data))
-        console.log(errors)
-    }, [data])
+    }, [data, touched])
 
 
     const changeHandler = event => {
@@ -28,35 +31,64 @@ const SignUp = () => {
         
     }
 
+    const focusHandler = event => {
+        setTouched({...touched, [event.target.name]: true })
+    }
+
+    const submitHandler = event => {
+        event.preventDefault();
+        
+        if (!Object.keys(errors).length) {
+            notify("You singed in successfully", "success")
+        } else {
+            notify("Invalid data!", "error")
+            setTouched ({
+                name: true,
+                email: true,
+                password: true,
+                confirmPassword: true,
+                isAccepted: true
+            })
+        }
+    }
+
+
+
     return ( 
         <div>
-            <form>
+            <form onSubmit={submitHandler}>
                 <h2>SignUp</h2>
                 <div>
                     <label>Name</label>
-                        <input type="text" name='name' value={data.name} onChange={changeHandler}  />
+                        <input type="text" name='name' value={data.name} onChange={changeHandler} onFocus={focusHandler}  />
+                        {errors.name && touched.name && <span>{errors.name}</span>}
                 </div>
                 <div>
                     <label>Email</label>
-                    <input type="text" name='email' value={data.email} onChange={changeHandler}  />
+                    <input type="text" name='email' value={data.email} onChange={changeHandler} onFocus={focusHandler}  />
+                    {errors.email && touched.email &&  <span>{errors.email}</span>}
                 </div>
                 <div>
                     <label>Password</label>
-                    <input type="password" name='password' value={data.password} onChange={changeHandler}  />
+                    <input type="password" name='password' value={data.password} onChange={changeHandler} onFocus={focusHandler}  />
+                    {errors.password && touched.password &&  <span>{errors.password}</span>}
                 </div>
                 <div>
                     <label>Confirm Password</label>
-                    <input type="password" name='confirmPassword' value={data.confirmPassword} onChange={changeHandler}  />
+                    <input type="password" name='confirmPassword' value={data.confirmPassword} onChange={changeHandler} onFocus={focusHandler}  />
+                    {errors.confirmPassword && touched.confirmPassword &&  <span>{errors.confirmPassword}</span>}
                 </div>
                 <div>
                     <label>I accept terms of privacy policy</label>
-                    <input type="checkbox" name='isAccepted' value={data.isAccepted} onChange={changeHandler}  />
+                    <input type="checkbox" name='isAccepted' value={data.isAccepted} onChange={changeHandler} onFocus={focusHandler}  />
+                    {errors.isAccepted && touched.isAccepted &&  <span>{errors.isAccepted}</span>}
                 </div>
                 <div>
                     <a href='#'>Login</a>
                     <button type="submit" >SignUp</button>
                 </div>
             </form>
+            <ToastContainer />
         </div>
     );
 };
